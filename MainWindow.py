8d9cbce2f949143d91c644a9c5e03a8d6c713d86
filipe <filipe.caporali@dynaton.com.br>
2021-01-04 21:02:05 -0300

    created bootstrap Grid inteerface for "main" screen

--- MainWindow.py
+++ MainWindow.py
@@ -1,9 +1,18 @@
 # greetings to https://www.delftstack.com/howto/python-tkinter/how-to-switch-frames-in-tkinter/
+from io import BytesIO
+from math import floor
+import random
+
+
+import PIL
+from PIL import Image
+from PIL.ImageTk import PhotoImage
+
 try:
     import Tkinter as tk
 except:
     import tkinter as tk
-from tkinter import Label, Button, Entry, messagebox
+from tkinter import Label, Button, Entry, messagebox, FLAT, BOTH
 from dbase import *
 from FileBase import *
 
@@ -20,7 +29,7 @@ class MainWindow(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.title('porkit')
-        self.resizable(0, 0)
+        self.resizable(0,0)
         self.config(bg=BGC)
         self._frame = None
         self.switch_frame(StartPage)
@@ -46,23 +55,21 @@ class LoginPage(tk.Frame):
     def login(self):
         self.master.switch_frame(AppViewPage).pack()
 
-
-
     def __init__(self, master):
         self.usernameS = tk.StringVar()
         self.passwordS = tk.StringVar()
 
         tk.Frame.__init__(self, master)
-        tk.Frame.configure(self, bg= BGC)
+        tk.Frame.configure(self, bg=BGC)
         tk.Label(self, text="Login", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
         tk.Button(self, text="Go back to start page", command=lambda: master.switch_frame(StartPage)).pack()
         user_label = Label(self, text="User", bg=BGC, fg="white")
         user_label.pack()
-        user_entry = Entry(self,textvariable=self.usernameS)
+        user_entry = Entry(self, textvariable=self.usernameS)
         user_entry.pack()
         pass_label = Label(self, text="Password", bg=BGC, fg="white")
         pass_label.pack()
-        pass_entry = Entry(self, show="*",textvariable=self.passwordS)
+        pass_entry = Entry(self, show="*", textvariable=self.passwordS)
         pass_entry.pack()
         login_button = Button(self, text="Login", command=self.validate)
         login_button.pack()
@@ -84,7 +91,6 @@ class LoginPage(tk.Frame):
             messagebox.showerror("Error", "Wrong Credentials")
 
 
-
 class RegisterPage(tk.Frame):
     def register(self):
         print('register')
@@ -95,26 +101,25 @@ class RegisterPage(tk.Frame):
         self.ConfpasswordS = tk.StringVar()
 
         tk.Frame.__init__(self, master)
-        tk.Frame.configure(self, bg= BGC)
+        tk.Frame.configure(self, bg=BGC)
         tk.Label(self, text="Register", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
         tk.Button(self, text="Go back to start page", command=lambda: master.switch_frame(StartPage)).pack()
         user_label = Label(self, text="User", bg=BGC, fg="white")
         user_label.pack()
-        user_entry = Entry(self,textvariable=self.usernameS)
+        user_entry = Entry(self, textvariable=self.usernameS)
         user_entry.pack()
         pass_label = Label(self, text="Password", bg=BGC, fg="white")
         pass_label.pack()
-        pass_entry = Entry(self, show="*",textvariable=self.passwordS)
+        pass_entry = Entry(self, show="*", textvariable=self.passwordS)
         pass_entry.pack()
         conf_label = Label(self, text="Confirmação", bg=BGC, fg="white")
         conf_label.pack()
-        conf_entry = Entry(self, show="*",textvariable=self.ConfpasswordS)
+        conf_entry = Entry(self, show="*", textvariable=self.ConfpasswordS)
         conf_entry.pack()
         register_button = Button(self, text="Register", command=self.add)
         register_button.pack()
 
         self.salt = bcrypt.gensalt()
-
 
     def add(self):
         username = self.usernameS.get()
@@ -147,8 +152,67 @@ class AppViewPage(tk.Frame):
         tk.Button(self, text="Go back to start page", command=lambda: master.switch_frame(StartPage)).pack()
         hello_label = Label(self, text="HELOOO MODAFOKA", bg=BGC, fg="white")
         hello_label.pack()
+        self.master.resizable(1, 1)
+        image = PIL.Image.open("mario.png")
+        # image.show()
+        filesarray = files.search_files_from_user((2,))
+
+
+
+        frame1 = tk.Frame(self, height=100, width=100, bg="GREY", borderwidth=2)
+        frame2 = tk.Frame(self, height=100, width=100, bg="BLUE", borderwidth=2)
+        frame1.pack()
+        frame2.pack()
+        # Create WidgetWrapper
+        widgetWrapper = tk.Text(frame1, wrap="char", borderwidth=0, highlightthickness=0, state="disabled",
+                                cursor="arrow")
+        # state = "disabled" is to disable text from being input by user
+        # cursor = "arrow" is to ensure when user hovers, the "I" beam cursor (text cursor) is not displayed
+
+        widgetWrapper.pack(fill="both", expand=True)
+
+        for iteration, x in enumerate(filesarray):
+            im = stream_toImage(x[3])
+            im.thumbnail((128, 128), Image.ANTIALIAS)
+            icon = PIL.ImageTk.PhotoImage(im)
+
+            icon = generate_thumbnail(x[3])
+
+            sound_btn = tk.Button(frame1, image=icon, relief=FLAT, command=lambda lan=x: stream_toImage(lan[3]).show())
+            sound_btn.image = icon
+            # sound_btn.pack()
+            additem(widgetWrapper, sound_btn)
+            print(iteration)
+
+
+
+
+
+def additem(widgetWrapper,item):
+    #item = Button(bd = 5, relief="solid", text="O", bg="red",width=20, height=20) #Create the actual widgets
+    widgetWrapper.window_create("end", window=item) #Put it inside the widget wrapper (the text)
+
+
+
+
+
+
+def stream_toImage(stream) -> Image:
+    stream = BytesIO(stream)
+    im = Image.open(stream).convert("RGBA")
+    stream.close()
+    return im
+
+
+def generate_thumbnail(xablau) -> PhotoImage:
+    im = stream_toImage(xablau)
+    im.thumbnail((128, 128), Image.ANTIALIAS)
+    icon = PIL.ImageTk.PhotoImage(im)
+    return icon
+
 
 
 if __name__ == "__main__":
     app = MainWindow()
+
     app.mainloop()
