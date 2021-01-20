ec33b88053c2afe012c4772f10974cc67aa27bb0
filipe <filipe.caporali@dynaton.com.br>
2021-01-20 18:55:45 -0300

    added configleton, and create tables on first run

--- porquinRun.py
+++ porquinRun.py
@@ -1,6 +1,22 @@
 from MainWindow import MainWindow
+from Configleton import Configleton
+from User import *
+from FileBase import *
 
+
+files = Filebase()
+files.createTable()
 if __name__ == "__main__":
-    app = MainWindow()
+    Configleton.shared_instance().set_cryptkey("senhasatanica")
+    #print(Configleton.shared_instance().get_cryptokey())
 
+    db = User()
+    db.createTable()
+    files = Filebase()
+    files.createTable()
+    db.finish()
+    files.finish()
+
+    app = MainWindow()
     app.mainloop()
+
