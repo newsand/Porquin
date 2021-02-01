8ec139c3859da825ff41cd1ea40c68ad41e558e6
filipe <filipe.caporali@dynaton.com.br>
2021-02-01 09:42:28 -0300

    hf

--- StartPage.py
+++ StartPage.py
@@ -11,6 +11,6 @@ class StartPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.configure(bg=BGC)
-        tk.Label(self, text="FileVault", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
-        tk.Button(self, text="Login", command=lambda: master.switch_frame(LoginPage.LoginPage)).pack()
-        tk.Button(self, text="Register", command=lambda: master.switch_frame(RegisterPage.RegisterPage)).pack()
+        tk.Label(self, text="Porquin", font=('Helvetica', 18, "bold"),bg=BGC,fg='white').pack(side="top", fill="x", pady=5)
+        tk.Button(self, text="Login", command=lambda: master.switch_frame(LoginPage.LoginPage)).pack( fill="x")
+        tk.Button(self, text="Register", command=lambda: master.switch_frame(RegisterPage.RegisterPage)).pack( fill="x")
